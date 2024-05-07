600a08887e5f791ee1b59c3ed73d3b676833eb44
Andrea Falconi <afalconi@e3r6p1.42roma.it>
2024-05-07 17:32:35 +0200

    aggiustamenti

--- CPP05/ex03/Intern.cpp
+++ CPP05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: afalconi <afalconi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 14:59:58 by afalconi          #+#    #+#             */
-/*   Updated: 2024/03/14 18:46:22 by afalconi         ###   ########.fr       */
+/*   Updated: 2024/05/07 17:32:01 by afalconi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,41 +58,3 @@ AForm*	Intern::makeForm(std::string FormName, std::string TargetName)
 
 	return(NULL);
 }
-
-//AForm* Intern::makeForm(std::string const& form_name, std::string const& target_name)
-//{
-//    std::string available_forms[3] = {"Presidential pardon", "Robotomy request", "Shrubbery creation"};
-//    int i = -1;
-//
-//    while (++i < 3)
-//    {
-//        if (form_name.compare(available_forms[i]) == 0)
-//            break ;
-//    }
-//
-//    try
-//    {
-//        switch (i + 1)
-//        {
-//            case 1:
-//                return (new PresidentialPardonForm(target_name));
-//                break;
-//            case 2:
-//                return (new RobotomyRequestForm(target_name));
-//                break;
-//            case 3:
-//                return (new ShrubberyCreationForm(target_name));
-//                break;
-//            default:
-//                throw FormNotFoundException();
-//        }
-//    }
-//    catch(const std::exception& e)
-//    {
-//        std::cerr << e.what();
-//        throw;
-//    }
-//
-//    return nullptr;
-//}
-
